0d1774a3f18e62d6e3618539be86bbc6c3371886
Battlelegend <schlosserfe@googlemail.com>
2024-06-10 15:54:16 +0200

    Major change.

--- src/main/java/de/edu/lmu/Main.java
+++ src/main/java/de/edu/lmu/Main.java
@@ -3,6 +3,6 @@ package de.edu.lmu;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello world!");
-        var pcg = new PCG_XSH_RR(5EEDx0);
+        var pcg = new PCG_XSH_RR(0x5EED);
     }
 }
