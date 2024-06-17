f5aa91564683e279126f7fe936c51826264a7322
Louisa Sommer <louisa.sommer@outlook.de>
2024-06-17 12:05:20 +0200

    6.3.2 (Lou): PCG-XSH-RS

---
+++ src/main/java/de/edu/lmu/PCG_XSH_RS.java
@@ -0,0 +1,28 @@
+package de.edu.lmu;
+
+public class PCG_XSH_RS implements PCG<PCG_XSH_RS>, PCGIntOutput {
+    private long state;
+
+    public PCG_XSH_RS(long seed) {
+        this.state = seed;
+    }
+
+    @Override
+    public void skipLong(long ulong) {
+        this.state = Util.skipLong(this.state, ulong);
+    }
+
+    @Override
+    public void skip(int uint) {
+        this.skipLong(uint);
+    }
+
+    @Override
+    public int nextInt() {
+        long xorshift   = this.state ^ (this.state >> 22);
+        int randomshift = (int) (this.state >> 61);
+        // probably we should have a next state method instead of using skip witch is slow!!
+        skip(1);
+        return (int) (xorshift >> (22 + randomshift));
+    }
+}
