e8afe8f394ab81bfd17c191b5a45dbf6ff63ff6e
Clarissa Kümhof <clarissakuemhof@MacBook-Air-von-Clarissa.local>
2024-06-20 12:16:11 +0200

    LinearComp.java code, but struggling with imports

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,18 @@ repositories {
 }
 
 dependencies {
+    implementation(files("libs/ssj-3.3.1.jar"))
+    implementation("ca.umontreal.iro.simul:ssj:3.3.2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.register<JavaExec>("runLinearComp") {
+    group = "application"
+    mainClass.set("de.edu.lmu.LinearComp")
+    classpath = sourceSets["main"].runtimeClasspath
 }
