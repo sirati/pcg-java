3cd588ec1b029bb5032e440bdad04592ce8c77f9
sirati <git.public@sirati.me>
2024-07-14 05:33:28 +0200

    fixed: forgot that builder wasnt done and messed up seed construction

--- src/test/java/de/edu/lmu/pcg/test/Util.java
+++ src/test/java/de/edu/lmu/pcg/test/Util.java
@@ -2,16 +2,16 @@ package de.edu.lmu.pcg.test;
 
 import de.edu.lmu.pcg.PCG;
 import de.edu.lmu.pcg.PCGBuilder;
+import de.edu.lmu.pcg.U128;
 import de.edu.lmu.pcg.services.PCGCtorService;
 
 import java.util.stream.Stream;
 
 public class Util {
-    @SuppressWarnings({"rawtypes", "unchecked"})
-    public static Stream<TestConstructor<?, ?>> rngCtorProvider() {
+    public static Stream<TestConstructor<?, U128>> rngCtorProvider() {
         return PCGCtorService.AVAILABLE_PCGS.values().stream()
                 .map(desc -> new PCGBuilder<>().type(desc.cls_PCG()))
-                .map(builder -> (TestConstructor<PCG, Number>) number
-                        -> ((PCGBuilder.StateConfigured) builder).seed(number).build());
+                .map(builder -> (TestConstructor<PCG, U128>) number
+                        -> builder.seedFromU128(number).build());
     }
 }
