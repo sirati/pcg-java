4e96b80b89efd9558ab507f4b0c545e4e394e415
Louisa Sommer <louisa.sommer@outlook.de>
2024-07-22 18:24:27 +0200

    removed unused code for better readability

--- src/vector21/java/de/edu/lmu/pcg/impl/vector/preview21/Util.java
+++ src/vector21/java/de/edu/lmu/pcg/impl/vector/preview21/Util.java
@@ -10,26 +10,6 @@ public final class Util {
     private Util() {
     }
 
-    /*
-    public static final long u128MultiplierHigh = 2549297995355413924L;
-    public static final long u128MultiplierLow = 4865540595714422341L;
-    public static final BigInteger u128Multiplier = BigInteger.valueOf(u128MultiplierHigh).shiftLeft(64)
-            .or(BigInteger.valueOf(u128MultiplierLow));
-    public static final long u128IncrementHigh = 6364136223846793005L;
-    public static final long u128IncrementLow = 1442695040888963407L;
-    public static final BigInteger u128Increment = BigInteger.valueOf(u128IncrementHigh).shiftLeft(64)
-            .or(BigInteger.valueOf(u128IncrementLow));
-    public static final BigInteger bigModulus = BigInteger.ONE.shiftLeft(127);
-
-    public static final long longMultiplier = 6364136223846793005L;
-    public static final long longIncrement = 1442695040888963407L;
-    public static final long longMod = 1L << 63;
-
-    public static final int intMultiplier = 1664525;
-    public static final int intIncrement = 1013904223;
-    public static final int intMod = 1 << 31;
-     */
-
     public static final VectorSpecies<Long> LONG_SPECIES = LongVector.SPECIES_PREFERRED;
     public static final byte LONG_COUNT = (byte) LONG_SPECIES.length();
     public static final short LONG_SIZE = (short) (LONG_COUNT * (short) 8);
@@ -40,19 +20,5 @@ public final class Util {
     public static final byte INT_COUNT = (byte) INT_SPECIES.length();
     public static final short INT_SIZE = (short) (INT_COUNT * (short) 4);
     public static final byte L2I_REDUCTION_FACTOR = (byte) (INT_COUNT / LONG_COUNT);
-
-
-    public static final LongVector u128MultiplierLow = LongVector.broadcast(LONG_SPECIES, de.edu.lmu.pcg.Util.u128MultiplierLow);
-    public static final LongVector u128MultiplierHigh = LongVector.broadcast(LONG_SPECIES, de.edu.lmu.pcg.Util.u128MultiplierHigh);
-    public static final LongVector u128IncrementLow = LongVector.broadcast(LONG_SPECIES, de.edu.lmu.pcg.Util.u128IncrementLow);
-    public static final LongVector u128IncrementHigh = LongVector.broadcast(LONG_SPECIES, de.edu.lmu.pcg.Util.u128IncrementHigh);
-
-
-    public static final LongVector longMultiplier = LongVector.broadcast(LONG_SPECIES, de.edu.lmu.pcg.Util.longMultiplier);
-    public static final LongVector longIncrement = LongVector.broadcast(LONG_SPECIES, de.edu.lmu.pcg.Util.longIncrement);
-    public static final LongVector longMod = LongVector.broadcast(LONG_SPECIES, de.edu.lmu.pcg.Util.longMod);
-    public static final IntVector intMultiplier = IntVector.broadcast(INT_SPECIES, de.edu.lmu.pcg.Util.intMultiplier);
-    public static final IntVector intIncrement = IntVector.broadcast(INT_SPECIES, de.edu.lmu.pcg.Util.intIncrement);
-    public static final IntVector intMod = IntVector.broadcast(INT_SPECIES, de.edu.lmu.pcg.Util.intMod);
 
 }
